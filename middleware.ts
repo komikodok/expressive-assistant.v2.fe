0f08e6742abe0be2a806edeeb73eb16da3b1a896
komikodok <komikodok666@gmail.com>
2025-11-05 22:02:08 +0700

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
- 
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request, secret: process.env.AUTH_SECRET })
-    const isAuthenticated = !!token
 
-    if (request.nextUrl.pathname.startsWith('/login')) {
-        if (isAuthenticated) {
-            return NextResponse.redirect(new URL('/', request.url))
-        }
-        return NextResponse.next()
-    }
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request, secret: process.env.AUTH_SECRET })
+  const isAuthenticated = !!token
+  const pathname = request.nextUrl.pathname
+
+  const publicRoutes = ['/login', '/register']
+
+  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route))
+
+  if (isAuthenticated && isPublicRoute) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
 }
- 
